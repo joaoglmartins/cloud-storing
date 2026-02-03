c702ffd98115dfae4f7ecc332c39a827fed70a9c
João Martins <joaogasparlopesmartins@gmail.com>
2026-02-03 19:41:25 +0000

    init backend

---
+++ backend/src/main/java/com/joaoglmartins/CloudStoringBE/CloudStoringBEApplication.java
@@ -0,0 +1,13 @@
+package com.joaoglmartins.CloudStoringBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStoringBEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudStoringBEApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/joaoglmartins/CloudStoringBE/CloudStoringBEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoglmartins.CloudStoringBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStoringBEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
